855c94f6bd8d3da565fd0c18833f156d4e04d2e6
Craig Tinney <66967571+craig-lofigames@users.noreply.github.com>
2024-01-09 13:44:51 +0000

    Create AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "Forgotten Construction Set" )]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany( "LoFi Games" )]
+#if BOATS
+[assembly: AssemblyProduct( "Forgotten Construction Set 2" )]
+#else
+[assembly: AssemblyProduct("Forgotten Construction Set")]
+#endif
+[assembly: AssemblyCopyright("© Copyright 2022, Copyright Lo-Fi Games Ltd. 2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6091ef05-c9d5-463a-a2ff-7a5e36553383")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.13.0.0")]
